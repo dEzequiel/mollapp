33779853fced91ad502cb3a9ab3ca3d5d51dc957
Ezequiel de la Rosa <erosarosario@cifpfbmoll.eu>
2022-02-11 00:17:51 +0100

    test(checkIfTasksAreInsideArray): add test to check if tasks collection size changed

---
+++ mollapp-application/src/test/java/edu/poniperro/administrator/ProgrammerTasksTest.java
@@ -0,0 +1,21 @@
+package edu.poniperro.administrator;
+import edu.poniperro.administrator.*;
+import edu.poniperro.filter.Authentication;
+import edu.poniperro.filter.Authorization;
+import edu.poniperro.targets.Vehicle;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+public class ProgrammerTasksTest {
+    
+    @Test
+    public void checkIfTasksAreInsideTasksArray() {
+        ProgrammerTasks programmer = new ProgrammerTasks(new Vehicle());
+        programmer.addTask(new Authentication());
+        programmer.addTask(new Authorization());
+
+        assertEquals(2, programmer.getTasks().size());
+    }
+
+}
