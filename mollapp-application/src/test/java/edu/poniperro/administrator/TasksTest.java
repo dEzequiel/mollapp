29d1f93169339060c4e0b00332ca91ed3198891a
Ezequiel de la Rosa <erosarosario@cifpfbmoll.eu>
2022-02-10 19:59:00 +0100

    test(presenceOfTarget): add test to check if the target is not null

---
+++ mollapp-application/src/test/java/edu/poniperro/administrator/TasksTest.java
@@ -0,0 +1,22 @@
+package edu.poniperro.administrator;
+import edu.poniperro.targets.*;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class TasksTest {
+    
+    @Test
+    public void presenceOfTarget() {
+        Tasks tasks = new Tasks();
+        Vehicle vehicle = new Vehicle();
+
+        tasks.setTarget(vehicle);
+
+        assertEquals(true, tasks.getTarget().isPresent());
+    }
+
+
+}   
